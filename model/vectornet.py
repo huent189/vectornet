9874836677fad8a85935296b11fcba29e440b6ca
huent189 <hueahgase189@gmail.com>
2021-02-05 09:51:06 +0000

    fix shared object in layer error

--- model/vectornet.py
+++ model/vectornet.py
@@ -10,9 +10,9 @@ class PathNet(nn.Module):
                  nn.ReLU(),
                  nn.BatchNorm2d(hidden_channel)]
         if repeat_num > 2:
-            mid_layer = [nn.Conv2d(hidden_channel, hidden_channel, kernel_size=k, stride=s, padding=p),
-                 nn.ReLU(),
-                 nn.BatchNorm2d(hidden_channel)] * (repeat_num - 2)
+            mid_layer = [nn.Sequential([nn.Conv2d(hidden_channel, hidden_channel, kernel_size=k, stride=s, padding=p),
+                                        nn.ReLU(),
+                                        nn.BatchNorm2d(hidden_channel)]) for _ in range((repeat_num - 2))]
         if last_activation == 'relu':
             last_layer = [nn.Conv2d(hidden_channel, 1, kernel_size=k, stride=s, padding=p),
                         nn.ReLU()]
